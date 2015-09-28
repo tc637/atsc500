1531cfb4270da19421f812a67139cf1e260974a3
Phil Austin <paustin@eos.ubc.ca>
2015-09-27 22:42:32 -0700

    working

--- a500_code/hist_numba.py
+++ a500_code/hist_numba.py
@@ -4,30 +4,16 @@ import numpy as np
 # nopython=True means an error will be raised
 # if fast compilation is not possible.
 @numba.jit(nopython=True)
-def fill_counts(row_centers,col_centers,row_indices,col_indices):
-    """
-          given bincenters for each row and column, and row_indices and
-          col_indices from searchsorted that give the row,col index of
-          each binned data point, return the counts in each 2d bin_col
-
-          input:  row_centers, col_centers   -- vectors giving the location
-                  of the center of each bin_col
-
-                  row_indices, col_indices  -- vectors giving the bin that
-                  every data point belongs in
-          output:  hist_array  -- 2d array of shape [len(row_centers),len(col_centers)]
-                  containing the number of datapoints that are in each row,column bin_column
-                  If there are no datapoints in a bin then the bin contains np.nan
-    """
-    num_colbins=col_centers.shape[0]
-    num_rowbins=row_centers.shape[0]
-    hist_array=np.zeros([num_rowbins, num_colbins], dtype=np.float)
-    num_y=row_indices.shape[0]
-    for n in range(num_y): #row_indices and col_indices both size of raw data
-        if col_indices[n] > 0 and row_indices[n] > 0 and \
-            col_indices[n] <= num_colbins and row_indices[n] <= num_rowbins:
-            bin_row=row_indices[n]-1 # '-1' to get the index of the bin center
-            bin_col=col_indices[n]-1
+def fill_counts(y_centers,x_centers,y_indices,x_indices,hist_array):
+    num_xbins=x_centers.shape[0]
+    num_ybins=y_centers.shape[0]
+    num_y=y_indices.shape[0]
+    num_x=x_indices.shape[0]
+    for n in range(num_y): #y_indices and x_indices both size of raw data
+        if x_indices[n] > 0 and y_indices[n] > 0 and \
+            x_indices[n] <= num_xbins and y_indices[n] <= num_ybins:
+            bin_row=y_indices[n]-1 # '-1' to get the index of the bin center
+            bin_col=x_indices[n]-1
             hist_array[bin_row, bin_col] += 1
     rows,cols=hist_array.shape
     for row in range(rows):
@@ -37,26 +23,15 @@ def fill_counts(row_centers,col_centers,row_indices,col_indices):
     return hist_array
 
             
-def hist2d(col_raw,row_raw,row_edges,col_edges):
-    """
-      Produce a 2-d histogram (for example, of temperature (y) vs.
-        vertical velocity (x) data)  binned into temperature,wvel
-        bins
-      input: row_raw,col_raw: data vectors of the row variable (temperature)
-             and the column variable (wvel)
-             col_edges, row_edges:  coordinates of the bin edges for each variables
-      returns:  counts,col_centers,row_centers
-
-      Example, given 10,000 temperature measurements to be binned into 20 bins, and
-               20,000 wvel measurements to be binned into 10 bins, return
-               counts as a [20,10]  array with the number of measurements that fall
-               into each bin
-    """
-    col_centers=(col_edges[:-1] + col_edges[1:])/2.
-    row_centers=(row_edges[:-1] + row_edges[1:])/2.
-    col_indices=np.asarray(np.searchsorted(col_edges, col_raw.flat, 'right'),dtype=np.int64)
-    row_indices=np.asarray(np.searchsorted(row_edges, row_raw.flat, 'right'),dtype=np.int64)
-    hist_array=fill_counts(row_centers,col_centers,row_indices,col_indices)
-    return hist_array,col_centers,row_centers
-
+def hist2d(x_raw,y_raw,x_edges,y_edges):
+    print('in numba')
+    x_centers=(x_edges[:-1] + x_edges[1:])/2.
+    y_centers=(y_edges[:-1] + y_edges[1:])/2.
+    num_xbins=int(len(x_centers))
+    num_ybins=int(len(y_centers))
+    x_indices=np.asarray(np.searchsorted(x_edges, x_raw.flat, 'right'),dtype=np.int64)
+    y_indices=np.asarray(np.searchsorted(y_edges, y_raw.flat, 'right'),dtype=np.int64)
+    hist_array=np.zeros([num_ybins, num_xbins], dtype=np.float)
+    hist_array=fill_counts(y_centers,x_centers,y_indices,x_indices,hist_array)
+    return hist_array,x_centers,y_centers
 
